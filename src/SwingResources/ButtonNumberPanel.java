0eed1c6183ef83b09ff7804a62b30b31d7e29d3c
SleepingFores7s <Daniel.Evensson@hotmail.com>
2025-10-29 23:43:20 +0100

    Change: I changed the class to instead use ArrayList to then use Collections and shuffle the array, then the objects get put into the panel.

--- src/SwingResources/ButtonNumberPanel.java
+++ src/SwingResources/ButtonNumberPanel.java
@@ -2,45 +2,31 @@ package SwingResources;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class ButtonNumberPanel extends JPanel {
 
+    ArrayList<JButton> buttonArray = new ArrayList<>();
+
     public ButtonNumberPanel() {
         setLayout(new GridLayout(4,4,4,4));
-//        setPreferredSize(new Dimension(sizeW, sizeH));
-        JButton button1 = new JButton("1");
-        JButton button2 = new JButton("2");
-        JButton button3 = new JButton("3");
-        JButton button4 = new JButton("4");
-        JButton button5 = new JButton("5");
-        JButton button6 = new JButton("6");
-        JButton button7 = new JButton("7");
-        JButton button8 = new JButton("8");
-        JButton button9 = new JButton("9");
-        JButton button10 = new JButton("10");
-        JButton button11 = new JButton("11");
-        JButton button12 = new JButton("12");
-        JButton button13 = new JButton("13");
-        JButton button14 = new JButton("14");
-        JButton button15 = new JButton("15");
-        JButton button16 = new JButton("");
 
-        add(button1);
-        add(button2);
-        add(button3);
-        add(button4);
-        add(button5);
-        add(button6);
-        add(button7);
-        add(button8);
-        add(button9);
-        add(button10);
-        add(button11);
-        add(button12);
-        add(button13);
-        add(button14);
-        add(button15);
-        add(button16);
+        //Empty space button
+        JButton buttonEmpty = new JButton("");
+        buttonArray.add(buttonEmpty);
+
+        //puts the buttons into the array
+        for (int i = 1; i <= 15; i++) {
+            buttonArray.add(new JButton(String.valueOf(i)));
+        }
+
+        //Shuffles the array
+        Collections.shuffle(buttonArray);
+
+        //adds the buttons to the panel
+        for (int i = 0; i < buttonArray.size(); i++) {
+            add(buttonArray.get(i));
+        }
     }
-
 }
