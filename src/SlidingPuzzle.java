c048ff684bd7c9e446cd9bcb1cb5c0133ee78415
SleepingFores7s <Daniel.Evensson@hotmail.com>
2025-10-29 11:33:41 +0100

    Initial commit

---
+++ src/SlidingPuzzle.java
@@ -0,0 +1,20 @@
+public class SlidingPuzzle {
+
+    /**
+     *- Make a frame/GUI for the game
+     *
+     * - make 15 numbers that will be placed inside the game in a 4x4 grid
+     *
+     * - Buttons 1 -> 15 must be randomly placed inside the square
+     *
+     * - Check positions when pressing a button
+     *      - each button must be capable of knowing its own position
+     *
+     * - Check if an empty square is beside the pressed button, and if there is, the button moves to the square.
+     *
+     * - If every number left -> right top -> bottom is 1 -> 15 then a Win message is displayed.
+     *
+     *
+     */
+
+}
