a9fc085dfdc4c379193854624d2eab9bd1541046
SleepingFores7s <Daniel.Evensson@hotmail.com>
2025-10-31 04:21:44 +0100

    Add: added a actionListener for the new Game button

--- src/SwingResources/ButtonNewGamePanel.java
+++ src/SwingResources/ButtonNewGamePanel.java
@@ -2,12 +2,21 @@ package SwingResources;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class ButtonNewGamePanel extends JPanel {
+public class ButtonNewGamePanel extends JPanel implements ActionListener {
 
     public ButtonNewGamePanel() {
        JButton newgameButton = new JButton("New Game");
        add(newgameButton);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+
 
     }
 }
