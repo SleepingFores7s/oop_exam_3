2b7224a87cb56ad3449b81ecba4173d116825653
SleepingFores7s <Daniel.Evensson@hotmail.com>
2025-11-01 23:19:31 +0100

    Add: added LocationRelative, and instaniated the variable panels in the class instead.

--- src/SwingResources/FrameWindow.java
+++ src/SwingResources/FrameWindow.java
@@ -4,23 +4,24 @@ package SwingResources;
 import javax.swing.*;
 import java.awt.*;
 
-public class FrameWindow extends JFrame {
+public class FrameWindow extends JFrame{
 
     final int SIZE_WIDTH = 500;
     final int SIZE_HEIGHT = 500;
+
+    PanelContainer panels;
 
     public void frameGUI(){
 
         setTitle("Sliding Pussle");
         setSize(SIZE_WIDTH,SIZE_HEIGHT);
         setResizable(false);
+        setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
 
-        
-        PanelContainer panels = new PanelContainer(SIZE_WIDTH, SIZE_HEIGHT);
+        panels = new PanelContainer(SIZE_WIDTH, SIZE_HEIGHT);
         add(panels, BorderLayout.CENTER);
-
 
 
         setVisible(true);
