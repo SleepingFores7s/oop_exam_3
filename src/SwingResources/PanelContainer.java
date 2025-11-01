6f5be55d551764553fcf217dc5c08caba0ec4d2c
SleepingFores7s <Daniel.Evensson@hotmail.com>
2025-11-01 23:21:00 +0100

    Fix/Add: added the button "New Game", and added a method to reset/re-shuffle the game.

--- src/SwingResources/PanelContainer.java
+++ src/SwingResources/PanelContainer.java
@@ -5,17 +5,33 @@ import java.awt.*;
 
 public class PanelContainer extends JPanel {
 
+    ButtonNumberPanel numberPanel;
+    JButton newGameButton = new JButton("New Game");
+
     public PanelContainer(int sizeW,int sizeH) {
         setLayout(new BorderLayout());
         setPreferredSize(new Dimension(sizeW, sizeH));
 
-        ButtonNumberPanel numberPanel = new ButtonNumberPanel();
+        //Number Buttons
+        numberPanel = new ButtonNumberPanel();
         add(numberPanel,BorderLayout.CENTER);
 
-        ButtonNewGamePanel newgamePanel = new ButtonNewGamePanel();
-        add(newgamePanel, BorderLayout.SOUTH);
+
+        //New Game button + listener
+        newGameButton.addActionListener(e -> {
+            resetGame();
+        });
+        add(newGameButton, BorderLayout.SOUTH);
 
 
+    }
+
+    //resets the game / re-shuffles
+    public void resetGame() {
+        numberPanel.removeAll();
+        numberPanel.shuffleButtons();
+        numberPanel.addToPanel();
+        numberPanel.revalidate();
     }
 
 }
