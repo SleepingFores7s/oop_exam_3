67b82f7174be00612e03ca2b9f7c94e1208a9312
SleepingFores7s <Daniel.Evensson@hotmail.com>
2025-11-02 00:04:53 +0100

    Comment: Added comments Color: Added color to empty button

--- src/SwingResources/PanelContainer.java
+++ src/SwingResources/PanelContainer.java
@@ -5,28 +5,38 @@ import java.awt.*;
 
 public class PanelContainer extends JPanel {
 
-    ButtonNumberPanel numberPanel;
+    //Essentials
+    private ButtonNumberPanel numberPanel;
     JButton newGameButton = new JButton("New Game");
 
     public PanelContainer(int sizeW,int sizeH) {
+
+        //Sets Layout
         setLayout(new BorderLayout());
+        setBorder(BorderFactory.createEmptyBorder(4,4,4,4));
+
+        //Sets panel size
         setPreferredSize(new Dimension(sizeW, sizeH));
 
         //Number Buttons
         numberPanel = new ButtonNumberPanel();
         add(numberPanel,BorderLayout.CENTER);
 
-
         //New Game button + listener
         newGameButton.addActionListener(e -> {
             resetGame();
+            System.out.println("Log: Game reset.");
         });
+
+        //Sets the color for New Game button
+        newGameButton.setBackground(Color.decode("#FFFAF0"));
+        newGameButton.setForeground(Color.decode("#005577"));
+
+        //Adds New Game button to panel
         add(newGameButton, BorderLayout.SOUTH);
-
-
     }
 
-    //resets the game / re-shuffles
+    //Resets the game / re-shuffles
     public void resetGame() {
         numberPanel.removeAll();
         numberPanel.shuffleButtons();
